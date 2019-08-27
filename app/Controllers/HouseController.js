673ea3777588573c8ff4cae87a424b73df52582c
Josiah Aubin <josiahaubin@gmail.com>
2019-08-27 15:27:48 -0600

    house get api working

--- app/Controllers/HouseController.js
+++ app/Controllers/HouseController.js
@@ -2,6 +2,23 @@ import HouseService from "../Services/HouseService.js";
 
 let _houseService = new HouseService()
 
-export default class HouseController {
+function _draw() {
+  let template = ``
+  let house = _houseService.House
 
+  house.forEach(h => {
+    template += h.Template
+  })
+
+  document.getElementById('house-cards').innerHTML = template
+}
+
+export default class HouseController {
+  constructor() {
+    //NOTE Register all subscribers
+    _houseService.addSubscriber('houses', _draw)
+
+    //NOTE Retrieve data
+    _houseService.getApiHouse()
+  }
 }
--- app/Services/HouseService.js
+++ app/Services/HouseService.js
@@ -1,6 +1,42 @@
+import House from "../Models/House.js";
 
+// @ts-ignore
+let _houseApi = axios.create({
+  baseURL: "http://bcw-sandbox.herokuapp.com/api/houses"
+})
 
+//Private
+let _state = {
+  houses: []
+}
+
+//NOTE methods to run when a given property in state changes
+let _subscribers = {
+  houses: []
+}
+
+function _setState(propName, data) {
+  //NOTE add the data to the state
+  _state[propName] = data
+  //NOTE run every subscriber function that is watching that data
+  _subscribers[propName].forEach(fn => fn());
+}
 
 export default class HouseService {
+  //NOTE adds the subscriber function to the array based on the property it is watching
+  addSubscriber(propName, fn) {
+    _subscribers[propName].push(fn)
+  }
 
+  get House() {
+    return _state.houses.map(h => new House(h))
+  }
+
+  getApiHouse() {
+    _houseApi.get()
+      .then(res => {
+        let houseData = res.data.data.map(h => new House(h))
+        _setState("houses", houseData)
+      })
+  }
 }
